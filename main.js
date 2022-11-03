f049e500ca5dafbbf049782a75df82e30f1e9c99
CTHall05 <charlestaylorhall05@gmail.com>
2022-11-03 16:18:28 +0000

    Taylor and Fliss commit first steps of the adventure!

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 /* 
 👉 Write your kata here!
+Write a function that adds a character to the beginning and end of a string.
 */
 
+
 //👉 Write the function your CodeWarriors will start with below here:
+ export function firstAndLast (string) {
+
+}
+
+// 👉 You are tasked with the assassination of the corrupt Politicean Makarth the craven!
+// Before we leave for our mission we must break open our bag to decide what equipment we need
+// You must change the string of your equipment into an array items
+export function equipmentBag (equipment) {
+    
+}
+
+// 👉 Hold on our bag has too many items inside of it! We must remove the last item!
+export function removeLastItem (equipmentInBag) {
+  
+}
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,46 @@
+// import {
+//     firstAndLast
+// } from './main.js';
+
+import { expect, test } from '@jest/globals';
+import { firstAndLast, equipmentBag, removeLastItem } from './main.js';
 //👉 Write your tests below here:
+
+// 1. First and last letter adding function
+// test("Testing our firstAndLast function", () => {
+//     const actual = firstAndLast("yes");
+//     const expected = "helloyesgoodbye";
+
+//     expect(actual).toBe(expected);
+// })
+
+test("Testing our firstAndLast function", () => {
+    const actual = firstAndLast("yes");
+    const expected = "1yes2";
+
+    expect(actual).toBe(expected);
+})
+
+// 2. Equipment function test
+test("Testing the equipmentBag function", () => {
+    const actual = equipmentBag("Rope, Blade, Cloak, Potion of Invisability");
+    const expected = ["Rope", "Blade", "Cloak", "Potion of Invisability"];
+
+    expect(actual).toStrictEqual(expected);
+})
+
+// test("Testing the equipmentBag function", () => {
+//     const actual = equipmentBag("1, 2, 3, 4");
+//     const expected = ["1", "2", "3", "4"];
+
+//     expect(actual).toStrictEqual(expected);
+// })
+
+
+// 3. Remove last item in backpack test
+test("Testing the removeLastItem function", () => {
+    const actual = removeLastItem(["Rope", "Blade", "Wabbajack", "Cat"]);
+    const expected = ["Rope", "Blade", "Wabbajack"];
+
+    expect(actual).toStrictEqual(expected);
+})
